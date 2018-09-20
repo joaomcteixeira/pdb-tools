13ec0f57432dc4a794ea784a9cbf9bc5671d4dbf
joaomcteixeira <joaomcteixeira@gmail.com>
2018-09-20 13:35:30 +0200

    changing to single function

--- pdb_add_chain.py
+++ pdb_add_chain.py
@@ -3,36 +3,28 @@
 """
 Adds chain IDs to PDB file based on residue number.
 
-usage: python pdb_add_chain.py <pdb file>
-examples:
-    python pdb_add_chain.py 1CTF.pdb
-    python pdb_add_chain.py -res 1CTF.pdb
-    python pdb_add_chain.py -ind "OXT HO3'" 1CTF.pdb
+usage: python pdb_chainbyres.py <pdb file>
+examples: python pdb_chainbyres.py 1CTF.pdb
 
 === DESCRIPTION:
 
 Adds chain labels, consecutively following ASCII.string.upper,
 in a multi-chain PDB file that lost the chain information.
+Chains are identified based on residue number, chain changes upon
+residue number restart.
     
     <- input
-    ATOM      1  N   GLY     1      52.630 104.940  39.430  1.00  0.00
-    ATOM      2  H1  GLY     1      51.700 105.330  39.400  1.00  0.00
+    ATOM   1732  CB  LYS   254      54.522  13.130  52.312  1.00 63.84           C  
+    ATOM   1733  CG  LYS   254      53.565  13.687  51.260  1.00 63.27           C  
+    ATOM   1735  N   PHE     2      -7.485  -6.367  19.069  0.50 64.74           N  
+    ATOM   1736  CA  PHE     2      -6.127  -6.372  18.438  0.70 63.43           C  
     
     -> output
-    ATOM      1  N   GLY A   1      52.630 104.940  39.430  1.00  0.00
-    ATOM      2  H1  GLY A   1      51.700 105.330  39.400  1.00  0.00
+    ATOM   1732  CB  LYS A 254      54.522  13.130  52.312  1.00 63.84           C  
+    ATOM   1733  CG  LYS A 254      53.565  13.687  51.260  1.00 63.27           C  
+    ATOM   1735  N   PHE B   2      -7.485  -6.367  19.069  0.50 64.74           N  
+    ATOM   1736  CA  PHE B   2      -6.127  -6.372  18.438  0.70 63.43           C  
     
-    
-    OPTIONS:
-    
-        -res: Recognizes chain break by residue number.
-            When residue number restarts, chain changes.
-        
-        -ind: Recognizes chain break by last atom identifier.
-              When last atom is met, changes chain starting on the next line.
-              Identifiers should be passed after OPTION as a space separated 
-              sequence wrapped as string.
-            
     
 Author: {0} ({1})
 
@@ -55,16 +47,42 @@ __email__ = "joaomcteixeira@gmail.com"
 USAGE = __doc__.format(__author__, __email__)
 
 
-def _add_chain(options, identifiers):
+def check_input(args):
+    """
+    Checks whether to read from stdin/file and validates user input/options.
+    """
+
+    if not len(args):
+        # Read from pipe
+        if not sys.stdin.isatty():
+            pdbfh = sys.stdin
+        else:
+            sys.stderr.write(USAGE)
+            sys.exit(1)
+    elif len(args) == 1:
+        # File
+        if not os.path.isfile(args[0]):
+            sys.stderr.write('File not found: ' + args[0] + '\n')
+            sys.stderr.write(USAGE)
+            sys.exit(1)
+        pdbfh = open(args[0], 'r')
+    else:
+        sys.stderr.write(USAGE)
+        sys.exit(1)
+
+    return pdbfh
+    
+
+def _add_chain_by_res(pdbfh):
     """Runs over each line of the PDB."""
     yield None
 
 if __name__ == '__main__':
     # Check Input
-    option, identifiers = check_option(sys.argv[1:])
+    pdbfh = check_input(sys.argv[1:])
 
     # Do the job
-    new_pdb = _add_chain(option, identifiers)
+    new_pdb = _add_chain_by_res(pdbfh)
 
     try:
         sys.stdout.write(''.join(new_pdb))
